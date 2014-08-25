0465d4f5f498f5aa7bbf15a8e8955d62943f7fca
Norbert Sebők <seboknorbi@gmail.com>
2014-08-25 09:32:13 +0200

    FORM_NAMES in config

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 
+VERSION = "2.3"
+
 API_URL = "http://127.0.0.1:5000"
 
 LOGO_PATH = "images/logo.png"
 DB_FOLDER = "db"
 
-VERSION = '2.3'
+FORM_NAMES = [
+    "Type alpha",
+    "Type beta",
+    "Type gamma"
+    ]
