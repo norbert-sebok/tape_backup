2cb6856ce3806e14ae44286e605cba79a6dc97d6
Norbert Sebők <seboknorbi@gmail.com>
2014-08-27 09:57:04 +0200

    comments in config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 
+# The version of the current application
 VERSION = "2.3"
 
+# The URL of the API server, including the port
 API_URL = "http://127.0.0.1:5000"
 
+# The path of the logo image
 LOGO_PATH = "images/logo.png"
+
+# The folder of the database file(s)
 DB_FOLDER = "db"
